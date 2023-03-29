54d0af4bc616ecb2e46c8a1da560e60604972bc4
arzuakov@yahoo.com <arzuakov@yahoo.com>
2023-03-29 11:14:21 +0300

    w29.03.2023

--- server/src/entity/weapons/rare.ts
+++ server/src/entity/weapons/rare.ts
@@ -1,5 +1,5 @@
 import { ICarComponent } from '../../interfaces/carComponent.interface'
-import { B1Aviator } from '../hardware/common'
+import { B1Aviator, CarJack, R1Breese, RS1Ruby } from '../hardware/common'
 import { P54MAccord } from './common'
 
 export class AC43Rapier implements ICarComponent {
@@ -17,8 +17,116 @@ export class AC43Rapier implements ICarComponent {
   getAllScrapMetal = () => {
     let scrapMetalCount = this.getScrapMetal()
     for (let item of this.ingredients) {
-      scrapMetalCount += item.getScrapMetal() 
+      scrapMetalCount += item.getScrapMetal()
     }
     return scrapMetalCount
   }
 }
+
+export class AD12Falcon implements ICarComponent {
+  name = 'АД-12 Сокол'
+  rarity = 'rare'
+  benchCost = 3
+  scrapMetal = 650
+  copper = 130
+  ingredients = [new RS1Ruby(), new P54MAccord()]
+
+  getScrapMetal = () => {
+    return this.scrapMetal
+  }
+
+  getAllScrapMetal = () => {
+    let scrapMetalCount = this.getScrapMetal()
+    for (let item of this.ingredients) {
+      scrapMetalCount += item.getScrapMetal()
+    }
+    return scrapMetalCount
+  }
+}
+
+export class Borer implements ICarComponent {
+  name = 'Бур'
+  rarity = 'rare'
+  benchCost = 3
+  scrapMetal = 650
+  copper = 130
+  ingredients = [new CarJack(), new R1Breese()]
+
+  getScrapMetal = () => {
+    return this.scrapMetal
+  }
+
+  getAllScrapMetal = () => {
+    let scrapMetalCount = this.getScrapMetal()
+    for (let item of this.ingredients) {
+      scrapMetalCount += item.getScrapMetal()
+    }
+    return scrapMetalCount
+  }
+}
+
+export class Vector implements ICarComponent {
+  name = 'Вектор'
+  rarity = 'rare'
+  benchCost = 3
+  scrapMetal = 650
+  copper = 130
+  ingredients = [new P54MAccord(), new CarJack()]
+
+  getScrapMetal = () => {
+    return this.scrapMetal
+  }
+
+  getAllScrapMetal = () => {
+    let scrapMetalCount = this.getScrapMetal()
+    for (let item of this.ingredients) {
+      scrapMetalCount += item.getScrapMetal()
+    }
+    return scrapMetalCount
+  }
+}
+
+[
+  {
+    name: 'Sledgehammer',
+    scrapMetal: 650,
+    copper: 130,
+    ingredients: ['Lupara', 'Car jack']
+  },
+  {
+    name: 'Little Boy 6LB',
+    scrapMetal: 650,
+    copper: 130,
+    ingredients: ['Avenger 57mm', 'Fuel barrel']
+  },
+  {
+    name: 'Wasp',
+    scrapMetal: 650,
+    copper: 130,
+    ingredients: ['Fuel barrel', 'Avenger 57mm']
+  },
+  {
+    name: 'Spitfire',
+    scrapMetal: 650,
+    copper: 130,
+    ingredients: ['Lupara', 'B-1 Aviator']
+  },
+  {
+    name: 'ST-M23 Defender',
+    scrapMetal: 650,
+    copper: 130,
+    ingredients: ['LM-54 Chord', 'R-1 Breeze']
+  },
+  {
+    name: 'Judge 76mm',
+    scrapMetal: 650,
+    copper: 130,
+    ingredients: ['Medium wheel ST', 'Avenger 57mm']
+  },
+  {
+    name: 'DT Cobra',
+    scrapMetal: 650,
+    copper: 130,
+    ingredients: ['Lupara', 'Radio']
+  }
+]
--- server/src/main.ts
+++ server/src/main.ts
@@ -4,26 +4,36 @@ import axios from 'axios'
 const aC43Rapier = new AC43Rapier()
 console.log(aC43Rapier.getAllScrapMetal());
 
-async function getItem(itemId: number) {
-  const arr = []
-  const res = await axios.get('https://crossoutdb.com/api/v1/recipe-deep/' + itemId).then(res => {
-    const item = {
-      name: '',
-      scrapMetal: 0,
-      copper: 0,
-      ingredients: []
+async function getItem(itemIds: number[]) {
+
+  class Item {
+    name: string
+    scrapMetal: number
+    copper: number
+    ingredients: string[]
+    constructor(name: string, scrapMetal: number, copper: number, ingredients: string[]) {
+      this.name = name
+      this.scrapMetal = scrapMetal
+      this.copper = copper
+      this.ingredients = ingredients
     }
-    console.log('name: ', res.data.item.name);
-    console.log('scrapMetal - ', res.data.recipe.recipe.ingredients[0].number)
-    console.log('copper - ', res.data.recipe.recipe.ingredients[1].number)
-    console.log(' ');
-    console.log('name - ', res.data.recipe.recipe.ingredients[2].item.name)
-    console.log('scrapMetal - ', res.data.recipe.recipe.ingredients[2].ingredients[0].number)
-    console.log('copper - ', res.data.recipe.recipe.ingredients[2].ingredients[1].number)
-    console.log(' ');
-    console.log('name - ', res.data.recipe.recipe.ingredients[3].item.name)
-    console.log('scrapMetal - ', res.data.recipe.recipe.ingredients[3].ingredients[0].number)
-    console.log('copper - ', res.data.recipe.recipe.ingredients[3].ingredients[1].number)
-  })
+  }
+
+  const arr: Item[] = []
+
+  for (const itemId of itemIds) {
+    await axios.get('https://crossoutdb.com/api/v1/recipe/' + itemId).then(res => {
+
+      const name = res.data.recipe.item.name
+      const scrapMetal = res.data.recipe.ingredients[0].number
+      const copper = res.data.recipe.ingredients[1].number
+      let ingredients: string[] = []
+      ingredients.push(res.data.recipe.ingredients[2].item.name)
+      ingredients.push(res.data.recipe.ingredients[3].item.name)
+      const item = new Item(name, scrapMetal, copper, ingredients)
+      arr.push(item)
+    })
+  }
+  console.log(arr);
 }
-getItem(44)
+getItem([94, 34, 51, 7, 2, 61, 44, 4, 74, 70, 123])
