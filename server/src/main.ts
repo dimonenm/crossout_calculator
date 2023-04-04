4019380f6c4d09e82843599e25730d9a1d371dee
exp <arzuakov@yahoo.com>
2023-04-04 23:38:51 +0300

    h04.04.2023

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,12 +6,8 @@ console.log(aC43Rapier.getAllScrapMetal());
 
 async function getItem(itemIds: number[]) {
 
-  const arr: string[] = []
-
   for (const itemId of itemIds) {
     await axios.get('https://crossoutdb.com/api/v1/recipe/' + itemId).then(res => {
-      const str = `export class ${res.data.recipe.item.name} implements ICarComponent { name = '' rarity = 'rare' benchCost = 3 scrapMetal = ${res.data.recipe.ingredients[0].number} copper = ${res.data.recipe.ingredients[1].number} ingredients = ['${res.data.recipe.ingredients[2].item.name}', '${res.data.recipe.ingredients[3].item.name}'] getScrapMetal = () => { return this.scrapMetal} getAllScrapMetal = () => { let scrapMetalCount = this.getScrapMetal() for (let item of this.ingredients) { scrapMetalCount += item.getScrapMetal()} return scrapMetalCount}}`
-      arr.push(str)
 
 
       console.log(`export class ${res.data.recipe.item.name} implements ICarComponent {`);
@@ -38,7 +34,6 @@ async function getItem(itemIds: number[]) {
       console.log(`}`);
     })
   }
-  // console.log(arr);
 
 }
 getItem([1060,1051,1419,1057,72,1476,457,1050,1049,1126,1371,763,373,598,475,1053,1054,1251,1065,73])
